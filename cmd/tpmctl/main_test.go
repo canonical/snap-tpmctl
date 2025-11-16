eac6f8aaa0adcd9c44b721f27c3caee0aff70910
Gabriele Belluardo <gabriele.belluardo@canonical.com>
2025-11-16 19:16:13 +0100

    test: add structure for table testing in main

--- cmd/tpmctl/main_test.go
+++ cmd/tpmctl/main_test.go
@@ -1,11 +1,36 @@
-package main_test
+package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
-type AppMock struct{}
+type mockApp struct{ err error }
 
-func (a AppMock) Run() error {
-	return nil
+func (m mockApp) Run() error { return m.err }
+
+func TestRun_Table(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		app            mockApp
+		wantExit       int
+		wantLogContain string
+	}{
+		"exit_with_success":      {app: mockApp{err: nil}, wantExit: 0},
+		"exit_with_error_code_1": {app: mockApp{err: errors.New("foo")}, wantExit: 1, wantLogContain: "foo"},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			if tc.wantExit != 0 {
+			}
+
+			if got := run(tc.app); got != tc.wantExit {
+				t.Fatalf("run() = %d, want %d", got, tc.wantExit)
+			}
+		})
+	}
 }
-
-func TestRun(t *testing.T) {}
