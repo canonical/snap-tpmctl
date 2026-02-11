d01f7b33d94adb59675c14a43a5c9946002dc6f7
Didier Roche <didier.roche@canonical.com>
2026-02-11 15:10:41 +0100

    First cmd/ test
    Co-authored-by: Dennis Loose <dennis.loose@canonical.com>
    Co-authored-by: Gabriele Belluardo <gabriele.belluardo@canonical.com>
    Co-authored-by: Matthew Hagemann <matt.hagemann@canonical.com>

---
+++ cmd/tpmctl/cmd/cmd_test.go
@@ -0,0 +1,23 @@
+package cmd_test
+
+import (
+	"testing"
+
+	"github.com/canonical/snap-tpmctl/cmd/tpmctl/cmd"
+	"github.com/canonical/snap-tpmctl/internal/testutils"
+	"github.com/matryer/is"
+)
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+	is := is.New(t)
+
+	ctx, logs := testutils.TestLoggerWithBuffer(t)
+
+	app := cmd.New([]string{"--help"})
+
+	err := app.Run(ctx)
+	is.NoErr(err)
+
+	is.True(logs.Len() == 0) // No logs printed by default
+}
