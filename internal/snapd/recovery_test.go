da47bd7f2bb0fa28aed72f7cd5332fa3d1107992
Gabriele Belluardo <gabriele.belluardo@canonical.com>
2026-03-04 11:49:33 +0100

    test: add tests for `GenerateRecoveryKey` api

--- internal/snapd/recovery_test.go
+++ internal/snapd/recovery_test.go
@@ -5,8 +5,39 @@ import (
 	"testing"
 
 	snapdtestutils "github.com/canonical/snap-tpmctl/internal/snapd/testutils"
+	"github.com/canonical/snap-tpmctl/internal/testutils/golden"
 	"github.com/matryer/is"
 )
+
+func TestGenerateRecoveryKey(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		wantErr bool
+	}{
+		"Returns_recovery_key_result": {},
+
+		"Error_on_invalid_result":           {wantErr: true},
+		"Error_on_snapd_call_returning_400": {wantErr: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			is := is.New(t)
+			c := snapdtestutils.NewMockSnapdServer(t, "/v2/system-volumes")
+
+			got, err := c.GenerateRecoveryKey(context.Background())
+			if tc.wantErr {
+				is.True(err != nil)
+				return
+			}
+			is.NoErr(err)
+
+			golden.CheckOrUpdateYAML(t, got)
+		})
+	}
+}
 
 func TestCheckRecoveryKey(t *testing.T) {
 	t.Parallel()
