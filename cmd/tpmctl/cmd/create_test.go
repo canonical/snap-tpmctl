0cddbc3b9fb8184347371b3044814240d32ae518
Gabriele Belluardo <gabriele.belluardo@canonical.com>
2026-03-25 16:00:23 +0100

    test(cmd): add unit tests for `create-recovery-key` command and related test data

---
+++ cmd/tpmctl/cmd/create_test.go
@@ -0,0 +1,53 @@
+package cmd_test
+
+import (
+	"testing"
+
+	"github.com/canonical/snap-tpmctl/cmd/tpmctl/cmd"
+	cmdtestutils "github.com/canonical/snap-tpmctl/cmd/tpmctl/cmd/testutils"
+	snapdtestutils "github.com/canonical/snap-tpmctl/internal/snapd/testutils"
+	"github.com/canonical/snap-tpmctl/internal/testutils"
+	"github.com/canonical/snap-tpmctl/internal/tpm"
+	tpmtestutils "github.com/canonical/snap-tpmctl/internal/tpm/testutils"
+	"github.com/matryer/is"
+)
+
+func TestCreateKey(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		recoveryKeyName string
+
+		wantErr bool
+	}{
+		"Success_on_creting_recovery_key": {recoveryKeyName: "test"},
+
+		"Error_on_empty_name":            {wantErr: true},
+		"Error_on_unique_name":           {recoveryKeyName: "test-duplicate", wantErr: true},
+		"Error_on_creating_recovery_key": {recoveryKeyName: "test", wantErr: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			is := is.New(t)
+			ctx, logs := testutils.TestLoggerWithBuffer(t)
+
+			command := "create-recovery-key"
+
+			c := snapdtestutils.NewMockSnapdServer(t, ctx)
+			s := tpm.New(tpmtestutils.WithSnapdClient(c.Client))
+			app := cmd.New(cmdtestutils.WithSnapTPM(s), cmdtestutils.WithArgs(command, tc.recoveryKeyName))
+
+			err := app.Run(ctx)
+			if tc.wantErr {
+				is.True(err != nil)
+				return
+			}
+			is.NoErr(err)
+
+			is.True(logs.Len() == 0) // No logs printed by default
+		})
+	}
+}
