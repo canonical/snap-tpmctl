c60df1e56611cf7269a2ad4d9161ec6f0e0797af
Gabriele Belluardo <gabriele.belluardo@canonical.com>
2026-04-10 09:43:59 +0200

    test(cmd): add golden file for TestStatus to verify FDE status output

--- cmd/tpmctl/cmd/status_test.go
+++ cmd/tpmctl/cmd/status_test.go
@@ -1,14 +1,17 @@
 package cmd_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/canonical/snap-tpmctl/cmd/tpmctl/cmd"
 	cmdtestutils "github.com/canonical/snap-tpmctl/cmd/tpmctl/cmd/testutils"
 	snapdtestutils "github.com/canonical/snap-tpmctl/internal/snapd/testutils"
 	"github.com/canonical/snap-tpmctl/internal/testutils"
+	"github.com/canonical/snap-tpmctl/internal/testutils/golden"
 	"github.com/canonical/snap-tpmctl/internal/tpm"
 	tpmtestutils "github.com/canonical/snap-tpmctl/internal/tpm/testutils"
+	"github.com/canonical/snap-tpmctl/internal/tui"
 	"github.com/matryer/is"
 )
 
@@ -32,9 +35,16 @@ func TestStatus(t *testing.T) {
 
 			command := "status"
 
+			var out strings.Builder
+			tui := tui.New(nil, &out)
+
 			c := snapdtestutils.NewMockSnapdServer(t, ctx)
 			s := tpm.New(tpmtestutils.WithSnapdClient(c.Client))
-			app := cmd.New(cmdtestutils.WithSnapTPM(s), cmdtestutils.WithArgs(command))
+			app := cmd.New(
+				cmdtestutils.WithSnapTPM(s),
+				cmdtestutils.WithArgs(command),
+				cmdtestutils.WithTui(tui),
+			)
 
 			err := app.Run(ctx)
 			if testutils.CheckError(is, err, tc.wantErr) {
@@ -42,6 +52,8 @@ func TestStatus(t *testing.T) {
 			}
 
 			is.True(logs.Len() == 0) // No logs printed by default
+
+			golden.CheckOrUpdate(t, out.String()) // TestStatus returns the correct output
 		})
 	}
 }
