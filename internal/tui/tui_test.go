3a6ec6a0fa95f826536313e0ba71728bd733029e
Gabriele Belluardo <gabriele.belluardo@canonical.com>
2026-04-21 13:50:00 +0200

    test(tui): add golden test for reading recovery key and passphrase

--- internal/tui/tui_test.go
+++ internal/tui/tui_test.go
@@ -1,6 +1,7 @@
 package tui_test
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -10,8 +11,10 @@ import (
 	"time"
 	_ "unsafe" // Required for go:linkname directives
 
+	"github.com/canonical/snap-tpmctl/internal/testutils"
 	"github.com/canonical/snap-tpmctl/internal/testutils/golden"
 	"github.com/canonical/snap-tpmctl/internal/tui"
+	"github.com/creack/pty"
 	"github.com/matryer/is"
 )
 
@@ -60,6 +63,127 @@ func TestSpin(t *testing.T) {
 
 		golden.CheckOrUpdate(t, globalBuf.String())
 	})
+}
+
+func TestReadSecret(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		input      string
+		wantSecret string
+
+		wantReadErr bool
+		wantErr     bool
+	}{
+		"Success":           {},
+		"Success_backspace": {input: "test\bx\n", wantSecret: "tesx"},
+		"Success_ctrl_c":    {input: "\x03", wantSecret: ""},
+
+		"Fail_reading_input": {wantReadErr: true, wantErr: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			is := is.New(t)
+
+			validSecret := "mysecret"
+			if tc.input == "" {
+				tc.input = validSecret + "\n"
+				tc.wantSecret = validSecret
+			}
+
+			ptmx, tty, err := pty.Open()
+			is.NoErr(err)
+			defer ptmx.Close()
+			defer tty.Close()
+
+			if tc.wantReadErr {
+				tty = nil
+			}
+
+			var out strings.Builder
+			tt := tui.New(tty, &out)
+
+			done := make(chan struct{})
+			go func() {
+				defer close(done)
+				fmt.Fprint(ptmx, tc.input)
+			}()
+
+			secret, err := tt.ReadUserSecret("Enter passphrase: ")
+
+			if testutils.CheckError(is, err, tc.wantErr) {
+				return
+			}
+			is.NoErr(err)
+			is.Equal(secret, tc.wantSecret)
+
+			golden.CheckOrUpdate(t, out.String())
+		})
+	}
+}
+
+func TestReadRecoveryKey(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		input   string
+		wantKey string
+
+		wantReadErr bool
+		wantErr     bool
+	}{
+		"Success":                   {},
+		"Success_with_typed_hyphen": {input: "12345-12345\n", wantKey: "1234512345"},
+		"Success_backspace":         {input: "1234\bx2345\n", wantKey: "123x2345"},
+
+		"Fail_reading_input": {wantReadErr: true, wantErr: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			is := is.New(t)
+
+			// 40-digit key (8 groups of 5 digits)
+			validKey := "1234512345123451234512345123451234512345"
+			if tc.input == "" {
+				tc.input = validKey + "\n"
+				tc.wantKey = validKey
+			}
+
+			ptmx, tty, err := pty.Open()
+			is.NoErr(err)
+			defer ptmx.Close()
+			defer tty.Close()
+
+			if tc.wantReadErr {
+				tty = nil
+			}
+
+			var out strings.Builder
+			tt := tui.New(tty, &out)
+
+			done := make(chan struct{})
+			go func() {
+				defer close(done)
+				fmt.Fprint(ptmx, tc.input)
+			}()
+
+			key, err := tt.ReadRecoveryKey()
+
+			if testutils.CheckError(is, err, tc.wantErr) {
+				return
+			}
+			is.NoErr(err)
+			is.Equal(key, tc.wantKey)
+
+			golden.CheckOrUpdate(t, out.String())
+		})
+	}
 }
 
 func getEscapes(t *testing.T) string {
